bb21a75d566bd813dfd0119945023cc7ada5da2f
Ivo Miller <ivo.miller@red-gate.com>
2017-08-30 15:03:27 +0100

    add migration script for wip

---
+++ Migrations/0002_20170830-1500_ivo.miller.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="f8a791eb-c80b-4ee3-b847-06805e6042b2" />
+GO
+
+PRINT N'Creating [Application].[Abc]'
+GO
+CREATE TABLE [Application].[Abc]
+(
+[Id] [int] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK__Abc__384653F6] on [Application].[Abc]'
+GO
+ALTER TABLE [Application].[Abc] ADD CONSTRAINT [PK__Abc__384653F6] PRIMARY KEY CLUSTERED  ([Id])
+GO
