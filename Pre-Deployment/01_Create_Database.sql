95b40f1244d2ae09b36688204dca78cd0e092b32
Ivo Miller <ivo.miller@red-gate.com>
2017-08-30 14:44:48 +0100

    Fix bodge

---
+++ Pre-Deployment/01_Create_Database.sql
@@ -0,0 +1,12 @@
+﻿IF (DB_ID(N'$(DatabaseName)') IS NULL)
+BEGIN
+	PRINT N'Creating $(DatabaseName)...';
+END
+GO
+IF (DB_ID(N'$(DatabaseName)') IS NULL)
+BEGIN
+	CREATE DATABASE [$(DatabaseName)]; -- MODIFY THIS STATEMENT TO SPECIFY A COLLATION FOR YOUR DATABASE
+
+	ALTER DATABASE [$(DatabaseName)] ADD FILEGROUP whatever CONTAINS MEMORY_OPTIMIZED_DATA
+	ALTER DATABASE [$(DatabaseName)] ADD FILE (name='whatever', filename='C:\Program Files\Microsoft SQL Server\MSSQL13.SQL2016\MSSQL\DATA\$(DatabaseName)') TO FILEGROUP whatever
+END
